480b67e3c81aeb0abd5bac81f0a77c3beefd18f2
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-09-17 15:53:12 -0400

    added sessions and logout

--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const {restricted} = require('./users-helpers');
+const restricted = require('./users-helpers');
 const Users = require('./users-model');
 const router = express.Router();
 
@@ -28,6 +28,7 @@ router.post('/login', (req, res) => {
     Users.findBy({ username })
       .first()
       .then(user => {
+        req.session.user = user;
         if (user && bcrypt.compareSync(password, user.password)) {
           res.status(200).json({ message: `Welcome ${user.username}!` });
         } else {
@@ -54,5 +55,20 @@ router.get('/hash', (req, res) => {
     const hash = bcrypt.hashSync(name, 8); // use bcryptjs to hash the name
     res.send(`the hash for ${name} is ${hash}`);
   });
+
+router.get('/logout', (req,res) => {
+  if(req.session){
+    req.session.destroy(err => {
+      if(err){
+        res.status(500).json({message: 'you can checkout but you cannot leave'})
+      } else{
+        res.status(200).json({message: 'bye'})
+      }
+    });
+  } else {
+    res.status(200).json({message: 'already logged out'})
+  }
+
+})
 
 module.exports = router;
