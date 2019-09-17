97ba3034bdf370ef734e10a2f40e8603e0d3037d
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-09-17 15:53:12 -0400

    added sessions and logout

--- api/server.js
+++ api/server.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const bcrpt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
 
 const UsersRouter = require('../users/users-router');
-
+const dbConnection = require('../data/db-config');
 const server = express();
+const sessionConfig = {
+    name: 'chocochip',
+    secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe',
+    cookie: {
+      maxAge: 1000 * 60 * 60, //in milliseconds
+      secure: false, //true means only send cookio over https
+      httpOnly: true, // true means JS has no access to the cookie
+  
+    },
+    resave: false,
+    saveUninitialized: true, //GGDPr compliance
+    store: new KnexSessionStore({
+      knex: dbConnection,
+      tablename: 'knexsessions',
+      sidfieldname: 'sessionid',
+      createtable: true,
+      clearInterval: 1000 * 60 * 30,
+    }),
+  }
+
+
+
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig))
 server.use('/api/users', UsersRouter);
 
 //sanity get
--- users/users-helpers.js
+++ users/users-helpers.js
@@ -1,25 +1,10 @@
-const bcrypt = require('bcryptjs');
 
-const Users = require('../users/users-model');
 
-module.exports = {
-    restricted
-}
-
-function restricted(req, res, next) {
-    let { username, password } = req.headers;
-
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'You shall not pass!' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+module.exports = (req, res, next) => {
+  if(req.session && req.session.user){
+    next();
+  } else {
+    res.status(401).json({message: 'you shall not pass!'});
+  }
 
 }
